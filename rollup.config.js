02c1abf6570529673cc9e8ee25dd1894eca80800
Alexey Avramenko <2822698@gmail.com>
2021-12-13 02:10:22 +0700

    fix build

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,11 @@
-import babel from "rollup-plugin-babel"
+import babel from "@rollup/plugin-babel"
 import { terser } from "rollup-plugin-terser"
-import typescript from "rollup-plugin-typescript2"
+import typescript from "@rollup/plugin-typescript"
+import { nodeResolve } from "@rollup/plugin-node-resolve"
+import commonjs from "@rollup/plugin-commonjs"
 import pkg from "./package.json"
+
+const extensions = [".js", ".ts"]
 
 export default {
     input: "src/index.ts",
@@ -30,8 +34,14 @@ export default {
         }
     ],
     plugins: [
+        typescript({ tsconfig: "./tsconfig.json" }),
+        babel({
+            babelHelpers: "bundled",
+            exclude: "node_modules/**",
+            extensions,
+        }),
+        nodeResolve({ extensions }),
+        commonjs({ extensions }),
         terser(),
-        babel(),
-        typescript(),
     ]
 }
